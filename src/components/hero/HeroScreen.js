df3396f1b19028c953a4135a5773cdab90c65390
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2021-12-08 03:37:07 -0500

    Pantalla de heroe individual

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { useParams, useNavigate ,Navigate } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroByID'
 
 export const HeroScreen = () => {
+    const {heroid}=useParams()
+    console.log(heroid)
+    const navigate=useNavigate()
+
+    const hero = getHeroById(heroid)
+    if(!hero){
+        return <Navigate to="/"/>
+    }
+    const handleReturn=(e)=>{
+        e.preventDefault()
+        navigate(-1)
+    }
+
+    const imagePath= `/assets/${hero.id}.jpg`
+
     return (
-        <div>
-            hero
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={imagePath} alt={hero.superhero} className="img-thumbnail"  />
+            </div>
+
+            <div className="col-8">
+                <h3>
+                    {hero.superhero}
+                </h3>
+                <ul className="list-group ">
+                    <li className="list-group-item">
+                        <b>Alter ego:</b> {hero.alter_ego}
+                    </li>
+                    <li className="list-group-item">
+                        <b>Publisher:</b> {hero.publisher}
+                    </li>
+                    <li className="list-group-item">
+                        <b>First appearance:</b> {hero.first_appearance}
+                    </li>
+                    <hr />
+                </ul>
+                <h5>Characteres</h5>
+                <p>{hero.characters}</p>
+                <button 
+                className="btn btn-outline-info"
+                onClick={handleReturn}
+                >
+                    Informacion
+                </button>
+            </div>
         </div>
     )
 }
